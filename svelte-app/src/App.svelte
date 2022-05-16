7abd47acbf67fb17126b74a444f68d7b1f3221b4
Mahesh Gaikwad <91421427+777ahesh@users.noreply.github.com>
2022-05-16 13:59:14 +0530

    first commit

---
+++ svelte-app/src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Header from './components/Header.svelte'
+	import Timer from './components/Timer.svelte'
+</script>
+
+<h1>
+	TEST
+</h1>
+<Header/>
+<Timer/>
---
+++ svelte-app/src/components/Timer.svelte
@@ -0,0 +1,6 @@
+<script>
+    import Progress from './Progress.svelte';
+</script>
+
+<h2>timer</h2>
+<Progress/>
